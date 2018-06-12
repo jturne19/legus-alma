6dd0618aca6c213368a7596d3848e1e925c5a862
Jordan <jturne19@uwyo.edu>
2018-06-12 17:45:55 -0600

    add

---
+++ science/science.py
@@ -0,0 +1,44 @@
+"""
+big 'ole python script that (hopefully) goes through the entire science procedure for give band 4 and band 7 fits files
+
+notes are given in science.md
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy
+from astropy.io import fits
+import os, sys, subprocess
+
+# define the band 4 and band 7 fits files to use
+b4_fits = 'band4.fits'
+b7_fits = 'band7.fits'
+
+# read in band 4 header and data 
+b4_hdulist = fits.open(b4_fits)
+b4_hdu = b4_hdulist[0]
+b4_header = b4_hdu.header
+b4_data = b4_hdu.data
+
+b4_bmaj = b4_header['bmaj'] * 3600.0 	# restoring beam major axis in arcsec
+b4_bmin = b4_header['bmin'] * 3600.0 	# restoring beam minor axis in arcsec
+
+
+# read in band 7 header and data
+b7_hdulist = fits.open(b7_fits)
+b7_hdu = b7_hdulist[0]
+b7_header = b7_hdu.header
+b7_data = b7_hdu.data
+
+b7_bmaj = b7_header['bmaj'] * 3600.0 	# restoring beam major axis in arcsec
+b7_bmin = b7_header['bmin'] * 3600.0 	# restoring beam minor axis in arcsec
+
+
+# use sextractor to extract the dust regions
+b4_sexcmd = 'sextractor ../%s -c config.band4.sex -seeing_FWHM %1.2f -back_type manual -back_value 0.0'%(b4_fits, b4_bmaj)
+b7_sexcmd = 'sextractor %s -c extract/config.band7.sex -seeing_FWHM %1.2f -back_type manual -back_value 0.0'%(b7_fits, b7_bmaj)
+
+try:
+	subprocess.call(b4_sexcmd.split())
+except OSError as e:
+	print(e)
