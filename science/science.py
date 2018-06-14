e3f08943e73baafd6c1ee1a9766a4cc920c5d6d9
jturne19 <jturne19@uwyo.edu>
2018-06-14 13:46:44 -0600

    update with making ds9 region files

--- science/science.py
+++ science/science.py
@@ -10,9 +10,19 @@ import astropy
 from astropy.io import fits
 import os, sys, subprocess
 
+# add scripts directory to the path
+sys.path.insert(0, '/uwpa2/turner/legus-alma/scripts/')
+from ds9_regions_from_sextractor import get_regions
+from get_sextractor_in_footprint import in_footprint
+
+
 # define the band 4 and band 7 fits files to use
 b4_fits = 'band4.fits'
 b7_fits = 'band7.fits'
+# define the other fits files needed
+b4_pbcoverage = 'band4.pbcoverage.fits'
+b7_pbcoverage = 'band7.pbcoverage.fits'
+
 
 # read in band 4 header and data 
 b4_hdulist = fits.open(b4_fits)
@@ -35,15 +45,27 @@ b7_bmin = b7_header['bmin'] * 3600.0 	# restoring beam minor axis in arcsec
 
 # use sextractor to extract the dust regions
 # need to run sextractor from physics network computer like uwpa
-b4_sexcmd = 'sex ../%s -c config.sex -catalog_name band4.cat -seeing_FWHM %1.2f -back_type manual -back_value 0.0'%(b4_fits, b4_bmaj)
-b7_sexcmd = 'sex ../%s -c config.sex -catalog_name band7.cat -seeing_FWHM %1.2f -back_type manual -back_value 0.0'%(b7_fits, b7_bmaj)
+b4_sexcmd = 'sex ../%s -c config.sex -catalog_name band4.cat -detect_thresh 1.5 -analysis_thresh 1.5 -seeing_FWHM %1.2f -pixel_scale 0.06 -back_type manual -back_value 0.0'%(b4_fits, b4_bmaj)
+b7_sexcmd = 'sex ../%s -c config.sex -catalog_name band7.cat -detect_thresh 1.0 -analysis_thresh 1.0 -seeing_FWHM %1.2f -pixel_scale 0.06 -back_type manual -back_value 0.0'%(b7_fits, b7_bmaj)
 
 # need to run sextractor from extract directory with the config files and default params things
 os.chdir('extract')
+
 # run sextractor commands
 try:
 	subprocess.call(b4_sexcmd.split())
 	subprocess.call(b7_sexcmd.split())
 except OSError as e:
 	print(e)
+
+# make new .cats that only have regions that lie within the 80% pbcoverage
+in_footprint('band4.cat', '../'+b4_pbcoverage)
+in_footprint('band7.cat', '../'+b7_pbcoverage)
+
+# next step is to make ds9 regions out of the sextractor .cat
+get_regions('band4.in_footprint.cat')
+get_regions('band7.in_footprint.cat')
+# outputs band4.reg and band7.reg
+
+# need to open band4.in_footprint.reg and band7.in_footprint.reg in ds9 and save as degree region files
 
