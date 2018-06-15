66cc45683aca562af8451eda1d97767fa0ec67fe
jturne19 <jturne19@uwyo.edu>
2018-06-15 16:09:27 -0600

    up

--- science/science.py
+++ science/science.py
@@ -9,11 +9,20 @@ import matplotlib.pyplot as plt
 import astropy
 from astropy.io import fits
 import os, sys, subprocess
+from astropy import constants as const
 
 # add scripts directory to the path
 sys.path.insert(0, '/uwpa2/turner/legus-alma/scripts/')
 from ds9_regions_from_sextractor import get_regions
 from get_sextractor_in_footprint import in_footprint
+from overlapping_regions import overlap
+
+"""
+if starting with new band 4 and band 7 fits files, you need to:
+	1. make sure to have pbcoverage files
+	2. take global.deg.reg and make new pixel files for band 4 and band 7 
+
+"""
 
 
 # define the band 4 and band 7 fits files to use
@@ -22,6 +31,12 @@ b7_fits = 'band7.fits'
 # define the other fits files needed
 b4_pbcoverage = 'band4.pbcoverage.fits'
 b7_pbcoverage = 'band7.pbcoverage.fits'
+
+
+def slope(x,y):
+	"""simple function to return the slope of a log line connecting two points
+	"""
+	return (np.log10(y[1]) - np.log10(y[0])) / (np.log10(x[1]) - np.log10(x[0]))
 
 
 # read in band 4 header and data 
@@ -65,7 +80,34 @@ in_footprint('band7.cat', '../'+b7_pbcoverage)
 # next step is to make ds9 regions out of the sextractor .cat
 get_regions('band4.in_footprint.cat')
 get_regions('band7.in_footprint.cat')
-# outputs band4.reg and band7.reg
+# outputs band4.in_footprint.reg and band7.in_footprint.reg
 
 # need to open band4.in_footprint.reg and band7.in_footprint.reg in ds9 and save as degree region files
+overlap('band4.in_footprint.deg.reg', 'band7.in_footprint.deg.reg')
+# outputs band4.overlap.deg.reg and band7.overlap.deg.reg
+
+# grab fluxes in regions
+# need the band frequencies
+#				   band 4, band 7
+freq = np.array([1.45E+11, 3.43E+11])
+wavel = const.c.value/freq
+
+
+b4_flux, b4_fluxerr, b4_bg = np.loadtxt('band4.overlap.cat', usecols=[0,1,3], unpack=True)
+b7_flux, b7_fluxerr, b7_bg = np.loadtxt('band7.overlap.cat', usecols=[0,1,3], unpack=True)
+
+# subtract out background
+b4_flux = b4_flux - b4_bg
+b7_flux = b7_flux - b7_bg
+
+# flux from sextractor is in Jy pix/beam so need to get rid of beam business
+beams = np.array([ np.pi/4.0 *b4_bmaj * b4_bmin, np.pi/4.0 * b7_bmaj*b7_bmin])
+pixel_size = 0.06**2
+pix_per_beam = beams/pixel_size
+
+# Jy
+flux = np.array([ b4_flux*freq[0]/pix_per_beam[0], b7_flux*freq[1]/pix_per_beam[1] ])
+flux_err = np.array([ b4_fluxerr*freq[0]/pix_per_beam[0], b7_fluxerr*freq[1]/pix_per_beam[1] ])
+
+slopes = slope(freq, flux)
 
