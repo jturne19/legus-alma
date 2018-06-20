bd95180e89bf266e5f2394aee2bc67fb1ec59cb2
jturne19 <jturne19@uwyo.edu>
2018-06-20 14:45:20 -0600

    some plotting stuff

--- science/science.py
+++ science/science.py
@@ -27,11 +27,12 @@ if starting with new band 4 and band 7 fits files, you need to:
 """
 # decide what you want:
 
-global_phot               = True		# perform global photometry?
-regions                   = True		# use sextractor to get dust regions and whatnot?
-fluxes                    = True		# calculate flux in dust regions and get slopes?
+global_phot               = False		# perform global photometry?
+regions                   = False		# use sextractor to get dust regions and whatnot?
+fluxes                    = False		# calculate flux in dust regions and get slopes?
 create_legus_region_files = False		# create ds9 region files from legus cluster catalog? (probably not necessary since those files are already on the github repo)
-closest_clusters          = True 		# find closest stellar clusters to dust regions?
+closest_clusters          = True		# find closest stellar clusters to dust regions?
+plot                      = False		# do some plotting?
 
 main_dir = '/uwpa2/turner/legus-alma/'
 
@@ -39,7 +40,7 @@ os.chdir(main_dir + 'science')
 
 # define the band 4 and band 7 fits files to use
 b4_fits = 'band4.fits'
-b7_fits = 'band7.fits'
+b7_fits = 'band7.feather.fits'
 # define the other fits files needed
 b4_pbcoverage = 'band4.pbcoverage.fits'
 b7_pbcoverage = 'band7.pbcoverage.fits'
@@ -60,10 +61,11 @@ def mbb_func_freq(freq, beta, T, sr_in_images):
 	return bb * freq * sr_in_images
 	
 
-global_phot = False
 # global photometry modified blackbody fit
 if global_phot:
 	from modBBfit import fit
+
+	print('\ndoing global photometry bidness \n')
 
 	os.chdir(main_dir + 'science/herschel/')
 	# first need pixel scale of the PACS and SPIRE images. units are MJy*pix/sr. we want Jy
@@ -108,15 +110,56 @@ if global_phot:
 
 	flux_sum = bb + ff + sync
 
-	### still need alma global photometry -- can't figure out how to do imcnts properly
+	# alma global phot
+	anet, anum_of_pix = np.loadtxt(main_dir+'/science/global/tables.asc', usecols=[1,2], unpack=True)
+	# net is in units Jy*pix/beam
+	b4_hdulist = fits.open(main_dir+ 'science/'+b4_fits)
+	b4_hdu = b4_hdulist[0]
+	b4_header = b4_hdu.header
+	b4_bmaj = b4_header['bmaj'] * 3600.0
+	b4_bmin = b4_header['bmin'] * 3600.0
 
+	b7_hdulist = fits.open(main_dir+'science/'+b7_fits)
+	b7_hdu = b7_hdulist[0]
+	b7_header = b7_hdu.header
+	b7_bmaj = b7_header['bmaj'] * 3600.0 
+	b7_bmin = b7_header['bmin'] * 3600.0 
+
+
+	beams = np.array([ np.pi/4.0 *b4_bmaj * b4_bmin, np.pi/4.0 * b7_bmaj*b7_bmin])
+	pixel_size = 0.06**2
+	pix_per_beam = beams/pixel_size	
+
+	alma_flux = anet * alma_freq / pix_per_beam	# Jy
+
+
+	# save alma data
+	np.savetxt(main_dir+'science/global/alma_global_flux.dat',np.transpose([const.c.value/alma_freq * 1e6, alma_flux*1e-26]), header='wavelength (micron) \t flux (W/m2)' )
 	# save herschel data
 	np.savetxt('herschel_flux.dat', np.transpose([const.c.value/freq * 1e6, flux*1e-26, flux_err]), header='wavelength (micron) \t flux (W/m2) \t 1 sigma error')
 	# save bb, ff, and sync data
 	np.savetxt('radiation.dat', np.transpose([wfit, bb, ff, sync, flux_sum]), header='wavelength (micron) \t BB (W/m2) \t F-F flux (W/m2) \t Synchrotron (W/m2) \t BB+FF+Sync (W/m2)')
 	np.savetxt('bb_params.dat', [beta, T], header='best fit parameters for the modified blackbody fit \nbeta, +error, -error \ntemperature, +error, -error')
 
+	# for testing how things look:
+	# read in data from the things above
+	wavel, bb, ff, sync, total = np.loadtxt(main_dir+'science/herschel/radiation.dat', unpack=True)
+	hwavel, hflux, herr = np.loadtxt(main_dir+'science/herschel/herschel_flux.dat', unpack=True)
+	awavel, aflux = np.loadtxt(main_dir + 'science/global/alma_global_flux.dat', unpack=True)
+
+	plt.figure()
+	plt.loglog(hwavel, hflux, 'ko')
+	plt.loglog(awavel, aflux, 'ro')
+	plt.loglog(wavel, total, 'k-')
+	plt.xlabel('Wavelength (micron)')
+	plt.ylabel(r'Flux (W/m$^2$)')
+	plt.show()
+
+
 if regions:
+
+	print('\ndoing dust region bidness \n')
+
 	# read in band 4 header and data 
 	b4_hdulist = fits.open(b4_fits)
 	b4_hdu = b4_hdulist[0]
@@ -138,7 +181,7 @@ if regions:
 	
 	# use sextractor to extract the dust regions
 	# need to run sextractor from physics network computer like uwpa
-	b4_sexcmd = 'sex ../%s -c config.sex -catalog_name band4.cat -detect_thresh 1.5 -analysis_thresh 1.5 -seeing_FWHM %1.2f -pixel_scale 0.06 -back_type manual -back_value 0.0'%(b4_fits, b4_bmaj)
+	b4_sexcmd = 'sex ../%s -c config.sex -catalog_name band4.cat -detect_thresh 1.0 -analysis_thresh 1.0 -seeing_FWHM %1.2f -pixel_scale 0.06 -back_type manual -back_value 0.0'%(b4_fits, b4_bmaj)
 	b7_sexcmd = 'sex ../%s -c config.sex -catalog_name band7.cat -detect_thresh 1.0 -analysis_thresh 1.0 -seeing_FWHM %1.2f -pixel_scale 0.06 -back_type manual -back_value 0.0'%(b7_fits, b7_bmaj)
 	
 	# need to run sextractor from extract directory with the config files and default params things
@@ -159,12 +202,21 @@ if regions:
 	get_regions('band4.in_footprint.cat')
 	get_regions('band7.in_footprint.cat')
 	# outputs band4.in_footprint.reg and band7.in_footprint.reg
-	
-	# need to open band4.in_footprint.reg and band7.in_footprint.reg in ds9 and save as degree region files
-	overlap('band4.in_footprint.deg.reg', 'band7.in_footprint.deg.reg')
-	# outputs band4.overlap.deg.reg and band7.overlap.deg.reg
+	done = False
+	while not done:
+		check = raw_input('Did you open the in_footprint.reg files and then save them as degree region files? [y/n] ')
+		if check == 'y'or check == 'yes' or check == 'Y' or check == 'Yes' or check == 'YES':
+			# need to open band4.in_footprint.reg and band7.in_footprint.reg in ds9 and save as degree region files
+			overlap('band4.in_footprint.deg.reg', 'band7.in_footprint.deg.reg')
+			# outputs band4.overlap.deg.reg and band7.overlap.deg.reg
+			done = True
+		else:
+			print('\nwell do it\n')
 
 if fluxes:
+
+	print('\ndoing flux continuum slope bidness \n')
+
 	os.chdir(main_dir + 'science/extract')
 
 	# grab fluxes in regions
@@ -208,6 +260,8 @@ if fluxes:
 
 if create_legus_region_files:
 
+	print('\nturning legus clusters into ds9 region files \n')
+
 	os.chdir(main_dir + 'science/legus')
 
 	ra, dec, cl = np.loadtxt('hlsp_legus_hst_acs-wfc3_ngc628-c_multiband_v1_padagb-mwext-avgapcor.tab', usecols=[3,4,33], unpack=True)
@@ -234,6 +288,9 @@ if create_legus_region_files:
 
 
 if closest_clusters:
+
+	print('\nfinding the closest stellar clusters to the dust blobs \n')
+
 	os.chdir(main_dir + 'science')
 	dustcoords, starcoords, age, mass, excess = nearest_cluster('extract/band4.overlap.deg.reg')
 
@@ -257,3 +314,75 @@ if closest_clusters:
  	np.savetxt('closest_clusters_props.average.dat', np.transpose([ang_sep_avg, phys_sep_avg, age_avg, mass_avg, excess_avg]), header='ang sep (arsec) \t physical sep (pc) \t age (yr) \t mass (solar mass) \t E(B-V)')
 	np.savetxt('closest_clusters_props.minimum.dat', np.transpose([ang_sep_min, phys_sep_min, age_min, mass_min, excess_min]), header='ang sep (arsec) \t physical sep (pc) \t age (yr) \t mass (solar mass) \t E(B-V)')
 
+if plot:
+
+	print('\ndoing some plotting bidness \n')
+
+	ang_sep_avg, phys_sep_avg, age_avg, mass_avg, excess_avg = np.loadtxt('closest_clusters_props.average.dat', unpack=True)
+	ang_sep_min, phys_sep_min, age_min, mass_min, excess_min = np.loadtxt('closest_clusters_props.minimum.dat', unpack=True)
+	slopes = np.loadtxt('slopes+errs.dat', usecols=[0])
+
+	plt.figure()
+	plt.semilogx(age_avg, slopes, 'ro')
+	# plt.loglog(age_avg, np.abs(slopes), 'ro')
+	plt.xlabel('Age (years)')
+	plt.ylabel('Dust Continuum Slope')
+	plt.title('3 closest clusters averaged')
+	plt.savefig('figs/age_avg.png')
+	# plt.show()
+
+	plt.figure()
+	plt.semilogx(age_min, slopes, 'ro')
+	plt.xlabel('Age (years)')
+	plt.ylabel('Dust Continuum Slope')
+	plt.title('closest cluster')
+	plt.savefig('figs/age_min.png')
+	# plt.show()
+
+	plt.figure()
+	plt.semilogx(mass_avg, slopes, 'ro')
+	# plt.loglog(mass_avg, -slopes, 'ro')	
+	plt.xlabel('Mass (solar masses)')
+	plt.ylabel('Dust Continuum Slope')
+	plt.title('3 closest clusters averaged')
+	plt.savefig('figs/mass_avg.png')
+	# plt.show()
+
+	plt.figure()
+	plt.semilogx(mass_min, slopes, 'ro')
+	plt.xlabel('Mass (solar masses)')
+	plt.ylabel('Dust Continuum Slope')
+	plt.title('closest cluster')
+	plt.savefig('figs/mass_min.png')
+	# plt.show()
+
+	plt.figure()
+	plt.semilogx(excess_avg, slopes, 'ro')
+	# plt.loglog(excess_avg, -slopes, 'ro')
+	plt.xlabel('E(B-V)')
+	plt.ylabel('Dust Continuum Slope')
+	plt.title('3 closest clusters averaged')
+	plt.savefig('figs/excess_avg.png')
+	# plt.show()
+
+	plt.figure()
+	plt.semilogx(excess_min, slopes, 'ro')
+	plt.xlabel('E(B-V)')
+	plt.ylabel('Dust Continuum Slope')
+	plt.title('closest cluster')
+	plt.savefig('figs/excess_min.png')
+	# plt.show()
+
+	plt.figure()
+	plt.semilogy(phys_sep_avg, age_avg, 'bo')
+	plt.xlabel('Physical Separation (pc)')
+	plt.ylabel('Age (yr)')
+	plt.title('3 closest clusters averaged')
+	plt.savefig('figs/age_sep_avg.png')
+
+	plt.figure()
+	plt.semilogy(phys_sep_min, age_min, 'bo')
+	plt.xlabel('Physical Separation (pc)')
+	plt.ylabel('Age (yr)')
+	plt.title('closest cluster')
+	plt.savefig('figs/age_sep_min.png')
